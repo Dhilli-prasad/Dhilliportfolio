b2eff47509cac1d52520513a7e9448e5c4c2c75f
G Dhilliprasad <dhilliprasad400@gmail.com>
2026-03-02 20:44:48 +0530

    Replace QR code generation with QRCode library

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-// Replace with your deployed GitHub Pages link after hosting
-const websiteURL = "https://yourusername.github.io/portfolio/";
-
-// Free QR Code API
-const qrAPI = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${websiteURL}`;
-
-document.getElementById("qrCode").src = qrAPI;
+new QRCode(document.getElementById("qrcode"), {
+    text: window.location.href,
+    width: 200,
+    height: 200
+});
